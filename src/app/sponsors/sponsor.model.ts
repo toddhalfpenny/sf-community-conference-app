6938cb656d2acb6ac883d3fb324b28b2c2826970
Todd Halfpenny <todd@toddhalfpenny.com>
2026-04-27 08:54:21 +0100

    Adding sponsor logos, and tidying up the list. I hope I've got them cacheing

--- src/app/sponsors/sponsor.model.ts
+++ src/app/sponsors/sponsor.model.ts
@@ -1,5 +1,6 @@
 export interface Sponsor {
   id: string; // The ID of the document.
+  logoUrl?: string; // The URL of the sponsor's logo.
   name: string; // The user friendly name.
   order: number; // The order in which the sponsor should be displayed.
   tiers: string[]; // The tiers of the sponsor (e.g. "Platinum", "Gold", "Silver").
