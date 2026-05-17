4a09ee22e7ecbd83b1047395955e14ee8de18ab5
Todd Halfpenny <todd@toddhalfpenny.com>
2026-05-17 10:41:30 +0100

    0.1.4 - Maps

--- src/app/maps/maps.page.ts
+++ src/app/maps/maps.page.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonTitle, IonToolbar, IonModal, IonButton } from '@ionic/angular/standalone';
+import { PinchZoomComponent } from '@meddv/ngx-pinch-zoom';
 
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.page.html',
   styleUrls: ['./maps.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonContent, IonHeader, IonMenuButton, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [PinchZoomComponent, IonButtons, IonContent, IonHeader, IonMenuButton, IonTitle, IonToolbar, CommonModule, FormsModule, IonModal, IonButton]
 })
 export class MapsPage implements OnInit {
+  protected isMapModalOpen = false;
+  protected selectedMap: string = '';
+  protected selectedMapName:  string = '';
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  protected openMapModal(mapToOpen: string, mapName: string) {
+    this.selectedMap = mapToOpen;
+    this.selectedMapName = mapName;
+    this.isMapModalOpen = true;
   }
 
 }
