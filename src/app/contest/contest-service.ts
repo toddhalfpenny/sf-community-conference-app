fa31a022186f52a92b0debd626f038f82114003f
Todd Halfpenny <todd@toddhalfpenny.com>
2026-05-31 17:32:55 +0100

    Update contest TTL

--- src/app/contest/contest-service.ts
+++ src/app/contest/contest-service.ts
@@ -23,8 +23,8 @@ const LOG_TAG = 'contest.servce';
 
 const CONTEST_DB_CONF = {
   // TTL: 1000 * 60 * 60, // 1 hour
-  // TTL: 1000 * 30 * 5, // 5 minutes
-  TTL: 1000 * 30, // 30 seconds
+  TTL: 1000 * 30 * 5, // 5 minutes
+  // TTL: 1000 * 30, // 30 seconds
   FETCHED_KEY: 'contest_fetched',
 }
 
